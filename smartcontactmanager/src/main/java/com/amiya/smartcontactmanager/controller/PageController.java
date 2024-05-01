44e265b7be1ef69b9a2cb1742b475e655545368e
Amiyakumarmishra <amiyamishra122001@gmail.com>
2024-05-01 13:15:14 +0530

    tailwind css set up

---
+++ smartcontactmanager/src/main/java/com/amiya/smartcontactmanager/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.amiya.smartcontactmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("This is home page handler");
+        model.addAttribute("userName", "Amiya");
+        return "home";
+    }
+
+}
